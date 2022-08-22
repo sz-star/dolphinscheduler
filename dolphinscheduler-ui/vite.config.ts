a95218712eb14e350b040d5ab4652b19b3783623
zhuhuaq <zhuhuaq@sutpc.com>
2022-08-22 18:30:28 +0800

    ci: 消除运行警告

--- dolphinscheduler-ui/vite.config.ts
+++ dolphinscheduler-ui/vite.config.ts
@@ -43,6 +43,7 @@ export default defineConfig({
     }
   },
   server: {
+    host: '0.0.0.0',
     proxy: {
       '/dolphinscheduler': {
         target: loadEnv('development', './').VITE_APP_DEV_WEB_URL,
